4df1130813ea13f21d3a13bbd9d2a1ec9efbadc7
jpalmat <jimmy.palma1990@gmail.com>
2022-09-30 13:58:44 -0500

    styles in the first page

--- src/navigations/HomeNavigator.js
+++ src/navigations/HomeNavigator.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 function HomeNavigator(props) {
   return (
     <ImageBackground 
     style={styles.background}
       source={require("../assets/images/pizza.jpeg")}>
-
+        <View style={styles.logoConatiner}>
+          <Image style={styles.logo} source={require("../assets/images/onStar.jpeg")}/>
+          <Text style={{backgroundColor: "white"}}>This is a test</Text>
+        </View>
+        <View style={styles.loginButton}></View>
+        <View style={styles.registerButton}></View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65"
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4"
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    
+  },
+  logoConatiner: {
+    position: "absolute",
+    top: 70,
+    alignItems:"center"
   }
 })
 
