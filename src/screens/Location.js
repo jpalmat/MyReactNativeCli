c38c765bdfac24421a436dfb328089676ce2d6b0
jpalmat <jimmy.palma1990@gmail.com>
2022-10-03 14:32:30 -0500

    markers in maps

--- src/screens/Location.js
+++ src/screens/Location.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps'
+import MapView, {Marker, Polyline} from 'react-native-maps'
+const myTattoo = require('../assets/images/myTattoo.webp')
+const mePhoto = require('../assets/images/me.jpg')
 
 function Location(props) {
+    const [userone, setUserone] = useState({
+        latitude: 42.517149,
+        longitude: -83.045274
+    });
+    const [usertwo, setUsertwo] = useState({
+        latitude: 42.417149,
+        longitude: -83.145274
+    });
     return (
         <View style={styles.container}>
-            <MapView style={styles.maps} />
+            <MapView 
+                style={styles.maps} 
+                initialRegion={{
+                    latitude: userone.latitude,
+                    longitude: userone.longitude,
+                    latitudeDelta: 0.09,
+                    longitudeDelta: 0.6
+                }}
+            >
+                {/* <Marker 
+                    coordinate={userone}
+                    // image={mePhoto}
+                /> */}
+                {/* <Marker coordinate={usertwo} ></Marker> */}
+            </MapView>
             <Text>Test</Text>
         </View>
     );
